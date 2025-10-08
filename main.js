a986f0c1ff7dffad79097add7c325f596cebdcaf
weirdwolfx <aadityapgoyal@gmail.com>
2025-10-08 18:00:06 +0530

    Add close btn to popup
    disable add book button when popup is open

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const addBookButton = document.querySelector('.add-book');
 const popupModal = document.querySelector('.popup-modal');
 const addBookForm = document.querySelector('.popup-modal > form');
 const backdrop = document.querySelector('.backdrop');
+const closePopupBtn = document.querySelector('.close-btn')
 
-addBookButton.addEventListener('click', () => {
+addBookButton.addEventListener('click', displayPopup);
+closePopupBtn.addEventListener('click', displayPopup);
+
+function displayPopup() {
+    addBookButton.toggleAttribute('disabled');
     popupModal.classList.toggle('show'); 
     backdrop.classList.toggle('show');
     addBookForm.reset();
-})
+}
