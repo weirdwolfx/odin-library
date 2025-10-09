ab5ec084c07d4fc37447ce37c6f74f839da19a37
weirdwolfx <aadityapgoyal@gmail.com>
2025-10-09 19:18:09 +0530

    Change submit-btn to not submit form on click

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const addBookButton = document.querySelector('.add-book');
 const popupModal = document.querySelector('.popup-modal');
 const addBookForm = document.querySelector('.popup-modal > form');
 const backdrop = document.querySelector('.backdrop');
-const closePopupBtn = document.querySelector('.close-btn')
+const closePopupBtn = document.querySelector('.close-btn');
+const submitBtn = document.querySelector('.submit-book-btn');
 
 addBookButton.addEventListener('click', displayPopup);
 closePopupBtn.addEventListener('click', displayPopup);
+submitBtn.addEventListener('click', displayPopup);
+
 
 function displayPopup() {
     addBookButton.toggleAttribute('disabled');
